558bd2ecd9a88cab2c7ea10d0fe7613566009e62
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-06-16 15:10:38 +0500

    task1 part 1 to 2 complete

--- js/script.js
+++ js/script.js
@@ -1,2 +1,9 @@
+
+// i. Get element of id “main-content” and assign them in a variable.
 let ele = document.getElementById("main-content");
+
+// ii. Display all child elements of “main-content” element.
+ele = ele.getElementsByTagName("p");
 console.log("Element ==== ", ele);
+
+// let ele_class = docum

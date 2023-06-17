407a7df8469061fd7f5300d098ea1fe084aed994
rabiayousuf187 <rabiayousuf187@gmail.com>
2023-06-17 11:22:00 +0500

    Task 2 part 3 done

--- js/task2.js
+++ js/task2.js
@@ -12,17 +12,20 @@ for(let ind = 0; ind < ele2.childNodes.length ; ind++){
 
 }
 
-// iii. Get all elements of class “render” and show their innerHTML
-// in browser.
-let ele_class = document.getElementsByClassName("render");
-// console.log("Child Elements ==== ", ele_class[0].innerHTML);
-for(let ind= 0 ; ind < ele_class.length ; ind++){
-    console.log(`Class Element: ${ind} inner HTML: ${ele_class[ind].innerHTML}`)
-}
+// iii. Update child node of element having id “lastName”.
+ele2.childNodes[0].nodeValue = 'Update Last Name : Zafar';
 
-// iv. Fill input value whose element id first-name using javascript.
-document.getElementById("first-name").value = "Aliena";
+// // iii. Get all elements of class “render” and show their innerHTML
+// // in browser.
+// let ele_class = document.getElementsByClassName("render");
+// // console.log("Child Elements ==== ", ele_class[0].innerHTML);
+// for(let ind= 0 ; ind < ele_class.length ; ind++){
+//     console.log(`Class Element: ${ind} inner HTML: ${ele_class[ind].innerHTML}`)
+// }
 
-// v. Repeat part iv for id ”last-name” and “email”.
-document.getElementById("last-name").value = "Fahad";
-document.getElementById("email").value = "aliena@gmail.com";
+// // iv. Fill input value whose element id first-name using javascript.
+// document.getElementById("first-name").value = "Aliena";
+
+// // v. Repeat part iv for id ”last-name” and “email”.
+// document.getElementById("last-name").value = "Fahad";
+// document.getElementById("email").value = "aliena@gmail.com";
